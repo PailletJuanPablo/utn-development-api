5e9b35688b53039efa201f484a025cd8c846a054
PailletJuanPablo <pailletjuanpablo@gmail.com>
2019-03-05 22:06:20 -0300

    testing login

--- app/Http/Controllers/ApiLoginController.php
+++ app/Http/Controllers/ApiLoginController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\User;
-use \Firebase\JWT\JWT;
 use GuzzleHttp\Client;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use \Firebase\JWT\JWT;
 
 class ApiLoginController extends Controller
 {
@@ -18,37 +18,35 @@ class ApiLoginController extends Controller
     public function login(Request $request)
     {
         $fileUrl = null;
-            $facebookBaseApi = "https://graph.facebook.com/me?fields=id,email,name,picture.type(large)&access_token=" . $request->token;
-            $response = $this->client->get($facebookBaseApi, ['verify' => false, 'exceptions' => false]);
-            $statusCode = $response->getStatusCode();
-            $content = json_decode($response->getBody()->getContents(), true);
-    
-            if ($statusCode != 200) {
-                return response()->json(["error" => 'Missing or invalid Token'], 400);
-            };
+        $facebookBaseApi = "https://graph.facebook.com/me?fields=id,email,name,picture.type(large)&access_token=" . $request->token;
+        $response = $this->client->get($facebookBaseApi, ['verify' => false, 'exceptions' => false]);
+        $statusCode = $response->getStatusCode();
+        $content = json_decode($response->getBody()->getContents(), true);
 
-            if ($content['picture']['data']['url']) {
-                $image = $content['picture']['data']['url'];
-               
-              //  
-            }
-            if($content['email']){
-                $email = $content['email'];
-            }
+        if ($statusCode != 200) {
+            return response()->json(["error" => 'Missing or invalid Token'], 400);
+        };
 
-            if($content['name']){
-                $name = $content['name'];
-            }
-        
+        if ($content['picture']['data']['url']) {
+            $image = $content['picture']['data']['url'];
+        }
+        if ($content['email']) {
+            $email = $content['email'];
+        }
+
+        if ($content['name']) {
+            $name = $content['name'];
+        }
 
         $user = User::where('email', $email)->first();
 
+        return $user;
         if (!$user) {
             $user = User::create([
                 'name' => $name,
                 'email' => $email,
                 'password' => Hash::make('secret'),
-                'image' => $image
+                'image' => $image,
             ]);
         }
 
@@ -58,13 +56,13 @@ class ApiLoginController extends Controller
         $dataToEncode = array(
             "id" => $user->id,
         );
-        
+
         $fcmEncoderKey = env('JWT_SECRET_KEY', 'secret');
         $accessToken = JWT::encode($dataToEncode, $fcmEncoderKey);
 
         return response()->json(
             [
-                "user"=> $user,
+                "user" => $user,
             ]
         );
     }
