3863259ad0f4550755a2d299ee65ec501322cb53
PailletJuanPablo <pailletjuanpablo@gmail.com>
2019-03-08 18:40:36 -0300

    Added middleware on base route

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index');
 
 Route::resource('schools', 'SchoolController');
 Route::resource('categories', 'CategoryController');
